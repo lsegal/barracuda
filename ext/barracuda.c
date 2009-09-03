9beba73d2f2699edd86ae8861439163a8c884488
Loren Segal <lsegal@soen.ca>
2009-09-03 04:43:40 -0400

    Bump to version 1.2

--- ext/barracuda.c
+++ ext/barracuda.c
@@ -42,7 +42,7 @@ static cl_context context = NULL;
 static size_t max_work_group_size = 65535;
 static int err;
 
-#define VERSION_STRING "1.1"
+#define VERSION_STRING "1.2"
 
 struct program {
     cl_program program;
