00f70d58a2fe93885a6d1014ed0813e63961088c
Justin W. Smith <justin.w.smith@gmail.com>
2014-02-05 19:08:52 -0500

    Corrected distance calculation.  Perform verification of the result

--- benchmarks/normalize.rb
+++ benchmarks/normalize.rb
@@ -5,22 +5,22 @@ require 'benchmark'
 
 include Barracuda
 
-def dist(*vec)
-  vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2]
+def dist(vec)
+  Math.sqrt(vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2] + vec[3] * vec[3])
 end
 
-def normalize(*vec)
-  d = dist(*vec)
+def normalize(vec)
+  d = dist(vec)
   vec.map {|c| c / d }
 end
 
 def norm_all(arr)
   out = []
-  0.step(arr.size - 1, 4) do |i|
-    vec = normalize(arr[i], arr[i + 1], arr[i + 2])
-    out.push(vec, 0.0)
+  0.step(arr.size - 3, 4) do |i|
+    vec = normalize(arr[i...(i + 4)])
+    out.push(*vec)
   end
-  out.flatten
+  out
 end
 
 srand
@@ -33,13 +33,24 @@ eof
 
 num_vecs = 1000000
 arr = []
-num_vecs.times { arr.push(rand, rand, rand, 0.0) }
+num_vecs.times { arr.push(rand, rand, rand, rand) }
 output = Buffer.new(arr.size).to_type(:float)
 
 Benchmark.bmbm do |x|
   # As done above in #norm_all
-  x.report("ruby") { norm_all(arr) }
+  x.report("ruby") { @result1 = norm_all(arr) }
   # As done above in Kernel
-  x.report("opencl") { prog.norm(output, arr, num_vecs) }
+  x.report("opencl") { @result2 = prog.norm(output, arr, num_vecs) }
+end
+
+if @result1.length != @result2.length
+  STDERR.puts "Invalid result"
+  exit(1)
+end
+
+error = @result1.size.times.find { |i| (@result1[i] - @result2[i]).abs > 0.000001 }
+if error
+  STDERR.puts "Error: #{@result1[error]} != #{@result2[error]}"
+  exit(1)
 end
 
